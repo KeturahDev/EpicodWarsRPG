0e6fbf41521ffe4b7b053cab4ba5048cb1e8ef5d
Keturah Howard <keturah.dev@gmail.com>
2020-04-15 13:57:41 -0700

    Create new ChangeState functions.
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-Authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-Authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>' did not match any file(s) known to git

--- src/EpicodWarsRPG.js
+++ src/EpicodWarsRPG.js
@@ -16,7 +16,16 @@ const initialGame =  {
   bossLifebar: 10,
 }
 
- const masterGame = storeState(initialGame);
+//  const masterGame = storeState(initialGame);
+ 
+   const startGame = storeState(initialGame); //only do once? - (runs player/boss objects through storeState)
+    const input = "James";
+    const assignName = changeState("playerName")(input);
+    const loadBoss = changeState("bossName")("Brooker T");
+  
+    //Act
+    const startGame2 = startGame(assignName); //changing state
+    const startGame3 = loadBoss(startGame2);
 
  export const changeState = (prop) => {  //prop = property
   return (value) => {           // value = value of property
@@ -26,10 +35,38 @@ const initialGame =  {
     })
   }
 }
+// const playerLBMax = player.lifebar (callWhenEnterFightMode)
+const enterWinState = changeState(playerLifebar)(10); //Add 10 points to playerLifeBar after win
+
+const classLesson = changeState(playerAttackStrength)(3); //Add 3 points to playerAttackStrength
+const standUp = changeState(playerAttackStrength)(1); //Add 1 point to playerAttackStrength
+
+
+
+
+
+
+// -Enter trainingMode: fully replenish lifebar
+// //training modes (buttons, each one triggers on e of these stateAlters)
+// -standUp increase playerAttackStrength by1
+// -classLesson increases playerAttackStrength by2
+
+// //fight modes
+// -attack hits lower healthbar (player => boss, boss => player)
+// -boss loses: 1. rassignBoss
+//              2. STRETCH: student: add stack ... increase lifebar max (onlyWithBattleVictory)
+//              3. enterTrainingRoom
+// -student loses: 
+//              1. Restart Game (state after uploading name)
+
+
 
 
 
 // brainstorm:
+
+// stretchgoals:
+// -confidence bool
 
 // // stretch goal -> students & teacher squads have training grounds
 // // squads fight bosses
@@ -59,5 +96,7 @@ const initialGame =  {
 // standUp (attackStrength)(value) +1
 // classLesson (attackStrength)(value) + 2  -- ex: babel(attackStrength)(1), webpack(attakcStrength)(2)
 // Stack (lifebar)(value) + 3
+
+
 // assign weapons: (langWeapon)("React")
 
