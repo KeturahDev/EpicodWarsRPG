1e0fb512e9b3fd197fd4ea9445e7ea1c0d1a8b24
Keturah Howard <keturah.dev@gmail.com>
2020-04-16 11:23:54 -0700

    pass bossAttack test

--- __tests__/EpicodWarsRPG.test.js
+++ __tests__/EpicodWarsRPG.test.js
@@ -23,7 +23,7 @@ describe("masterGame", () => {
     const bossLoaded = updateGame(loadBoss); 
      
     //Assert
-    expect(bossLoaded).toEqual({playerName: "James", playerAttackStrength: 1, playerLifebar: 5, bossName: "Brooker T", bossAttackStrength: 5, bossLifebar: 10});
+    expect(bossLoaded).toEqual({playerName: "James", playerAttackStrength: 1, playerLifebar: 5, bossName: "Brooker T", bossAttackStrength: 5, bossLifebar: 7});
   });
 });
 
@@ -74,27 +74,39 @@ describe("FightingMode", () => {
     const nameAssigned = updateGame(assignName);
     const bossLoaded = updateGame(loadBoss); 
     
-    // const classLesson = changeState("playerAttackStrength")(3);
-    // const standUp = changeState("playerAttackStrength")(1);
     const goToClassLesson = changeState("playerAttackStrength")(3);
     const goToStandUp = changeState("playerAttackStrength")(1);
     const playerWentToClass = updateGame(goToClassLesson);
-    console.log("after class:" , playerWentToClass.playerAttackStrength);
     const playerWentToStandUp = updateGame(goToStandUp);
-    console.log("after standup:" , playerWentToStandUp.playerAttackStrength);
-    //player attaackStrenght = 5
-    // boss lifebar = 7
-    // 5-7
     
     //Act
     const battleBegins = updateGame(playerGiveHit);
-    console.log("after giving hit PLAYERATTACK:" , playerWentToStandUp.playerAttackStrength);
-    console.log("after giving hit LIFEBAR:" , playerWentToStandUp.bossLifebar);
 
     //Assert
     expect(battleBegins.bossLifebar).toEqual(2);
   });
+  test("should decrement bossLifebar by 5", () => {
+    // Arrange
+    const initialGame =  { 
+      playerName: "",
+      playerAttackStrength: 1,
+      playerLifebar: 5, 
+      bossName: "",
+      bossAttackStrength: 5,
+      bossLifebar: 7,
+    }
+   
+    const updateGame = storeState(initialGame); 
+    const input = "James";
+    const assignName = changeState("playerName")(input);
+    const loadBoss = changeState("bossName")("Brooker T");
+    const nameAssigned = updateGame(assignName);
+    const bossLoaded = updateGame(loadBoss); 
+   
+    //Act
+    const battleBegins = updateGame(bossGiveHit);
+
+    //Assert
+    expect(battleBegins.playerLifebar).toEqual(0);
+  });
 });
-
-
-
